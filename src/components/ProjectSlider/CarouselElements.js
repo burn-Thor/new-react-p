5519fa2fada3357d809ad1d3df40cdff42f30329
burn-Thor <eothorburn@gmail.com>
2022-08-09 23:52:01 +0100

    carousel & navbar still buggy

---
+++ src/components/ProjectSlider/CarouselElements.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const CarouselBox = styled.div`
+    height: 350px;
+`
+
+export const Image = styled.img`
+    height: 60px;`
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ProjectsCarousel from '../components/ProjectSlider/ProjectsCarousel'
 
 const Projects = () => {
   return (
     <div>
-      <h2>Projects</h2>
+      <h2>Screenshots of some of my favourite parts of projects to date:</h2>
+      <ProjectsCarousel projects={ProjectsCarousel}/>
     </div>
   )
 }
